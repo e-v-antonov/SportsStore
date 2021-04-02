d907960dd12b86188936c4cb9a8d1572e0c71110
e-v-antonov <antonov.eugeny2012@yandex.ru>
2021-04-02 17:33:44 +0300

    Создано определение расширяющих методов состояния сеанса

---
+++ Controllers/CartController.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using SportsStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using SportsStore.Infrastructure;
+
+namespace SportsStore.Controllers
+{
+    /// <summary>
+    /// Класс контроллера для корзины покупок
+    /// </summary>
+    public class CartController : Controller
+    {
+        private IProductRepository repository;
+
+        public CartController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public RedirectToActionResult AddToCart(int productId, string returnUrl)
+        {
+            //TODO: Вынести в отдельный метод повторяющиеся куски кода 
+            Product product = repository.Products
+                .FirstOrDefault(p => p.ProductID == productId);
+
+            if (product != null)
+            {
+                Cart cart = GetCart();
+                cart.AddItem(product, 1);
+                SaveCart(cart);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToActionResult RemoveFormCart(int productId, string  returnUrl)
+        {
+            Product product = repository.Products
+                .FirstOrDefault(p => p.ProductID == productId); 
+
+            if (product != null)
+            {
+                Cart cart = GetCart();
+                cart.RemoveLine(product);
+                SaveCart(cart);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        private Cart GetCart()
+        {
+            Cart cart = HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();
+            return cart;
+        }
+
+        private void SaveCart(Cart cart)
+        {
+            HttpContext.Session.SetJson("Cart", cart);
+        }
+    }
+}
---
+++ Infrastructure/SessionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using Microsoft.AspNetCore.Http;
+namespace SportsStore.Infrastructure
+{
+    /// <summary>
+    /// Класс для расширяющих методов, которые предоставят доступ к данным состояния сеанса
+    /// </summary>
+    public static class SessionExtensions
+    {
+        public static void SetJson(this ISession session, string key, object value)
+        {
+            session.SetString(key, JsonConvert.SerializeObject(value));
+        }
+
+        public static T GetJson<T>(this ISession session, string key)
+        {
+            var sessionData = session.GetString(key);
+            return sessionData == null ? default(T) : JsonConvert.DeserializeObject<T>(sessionData);
+        }
+    }
+}
