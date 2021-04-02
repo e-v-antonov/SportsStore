36cb8123b4930a31b40c827c22b1fd76be6437e1
e-v-antonov <antonov.eugeny2012@yandex.ru>
2021-04-02 16:09:52 +0300

    Создана кнопка добавления товара в корзину

---
+++ Infrastructure/UrlExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace SportsStore.Infrastructure
+{
+    /// <summary>
+    /// Класс для расширяющего метода, который генерирует URL,
+    /// по которому браузер будет возвращаться после обновления корзины,
+    /// принимая во внимание строку запроса
+    /// </summary>
+    public static class UrlExtensions
+    {
+        public static string PathAndQuery(this HttpRequest request) =>
+            request.QueryString.HasValue
+            ? $"{request.Path}{request.QueryString}"
+            : request.Path.ToString();
+    }
+}
