87ddf94f2b21659fd5b8166836f01779fdbc75eb
e-v-antonov <antonov.eugeny2012@yandex.ru>
2021-04-02 15:54:29 +0300

    Было создано определение модели корзины

---
+++ Models/Cart.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace SportsStore.Models
+{
+    /// <summary>
+    /// Класс определения модели корзины
+    /// </summary>
+    public class Cart
+    {
+        private List<CartLine> lineCollection = new List<CartLine>();
+
+        public virtual void AddItem(Product product, int quantity)
+        {
+            CartLine line = lineCollection
+                .Where(p => p.Product.ProductID == product.ProductID)
+                .FirstOrDefault();
+
+            if (line == null)
+            {
+                lineCollection.Add(new CartLine
+                {
+                    Product = product,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+
+        public virtual void RemoveLine(Product product) =>
+            lineCollection.RemoveAll(l => l.Product.ProductID == product.ProductID);
+
+        public virtual decimal CompareTotalValue() =>
+            lineCollection.Sum(e => e.Product.Price * e.Quantity);
+
+        public virtual void Clear() => lineCollection.Clear();
+
+        //Свойство для обращения к содержимому корзины
+        public virtual IEnumerable<CartLine> Lines => lineCollection;
+    }
+
+    /// <summary>
+    /// Класс представляет товар выбранный покупателем
+    /// </summary>
+    public class CartLine
+    {
+        public int CartLineID { get; set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
