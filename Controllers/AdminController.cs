172e6509d2bdec77ee97de9388a2dcf6ec28a496
e-v-antonov <antonov.eugeny2012@yandex.ru>
2021-04-07 20:49:23 +0300

    Создани контроллер CRUD и реализовано представление списка

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using SportsStore.Models;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SportsStore.Controllers
+{
+    /// <summary>
+    /// Класс контроллера для управления каталогом товаров
+    /// </summary>
+    public class AdminController : Controller
+    {
+        private IProductRepository repository;
+
+        public AdminController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index() => View(repository.Products);
+    
+        
+    }
+}
