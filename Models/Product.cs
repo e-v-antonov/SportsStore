904754b5a476ecd494d301b3ad63a6a3ee0ee6c1
e-v-antonov <antonov.eugeny2012@yandex.ru>
2021-04-08 17:17:31 +0300

    Добавлена проверка достоверности модели

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SportsStore.Models
 {
@@ -9,13 +10,18 @@ namespace SportsStore.Models
     {
         public int ProductID { get; set; }
 
+        [Required(ErrorMessage = "Please enter a product name")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Please enter a description")]
         public string Description { get; set; }
 
         [Column(TypeName = "decimal(8, 2)")]
+        [Required(ErrorMessage = "Please enter a price")]
+        [Range(1, 9999999999, ErrorMessage = "Please enter a positive price")]
         public decimal Price { get; set; }
 
+        [Required(ErrorMessage = "Please specify a catefory")]
         public string Category { get; set; }
     }
 }
